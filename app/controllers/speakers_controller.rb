9ed378b79fddd17c87900193b24b0a38b82bbb82
mykeeouellet <mykee.o@icloud.com>
2020-05-01 11:23:01 -0700

    begin controller

--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -5,9 +5,10 @@ class SpeakersController < ApplicationController
     require 'json'
     
     def index
-
     end
     
+    @subscriptionKey = '2b9cb2c1a1e7424a9034683b34eacd7c'
+
     # First Step is to create the profile
     # Will return an Identification Id to Enroll
     def createProfile
@@ -15,20 +16,16 @@ class SpeakersController < ApplicationController
         uri.query = URI.encode_www_form({
         })
         request = Net::HTTP::Post.new(uri.request_uri)
-        # Request headers
         request['Content-Type'] = 'application/json'
-        # Request headers 
-        request['Ocp-Apim-Subscription-Key'] = '2b9cb2c1a1e7424a9034683b34eacd7c'
-        # Request body
+        request['Ocp-Apim-Subscription-Key'] = @subscriptionKey
         request.body = '{
             "locale":"en-us"
           }'
         response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
             http.request(request)
         end
-
         @identificationProfileId = response.body['identificationProfileId']
-        puts @identificationProfileId
+        return @identificationProfileId
     end
 
     #Create an Enrollment with the Identification 
@@ -36,22 +33,29 @@ class SpeakersController < ApplicationController
     def createEnrollment(identificationProfileId)    
     uri = URI('https://elevatorrecognition.cognitiveservices.azure.com/spid/v1.0/identificationProfiles/' + @identificationProfileId + '/enroll')
     uri.query = URI.encode_www_form({
-        # Request parameters
         'shortAudio' => '{false}'
     })
     request = Net::HTTP::Post.new(uri.request_uri)
-    # Request headers
     request['Content-Type'] = 'multipart/form-data'
-    # Request headers
-    request['Ocp-Apim-Subscription-Key'] = '{2b9cb2c1a1e7424a9034683b34eacd7c}'
-
-    # Request body ( Audio File need to be put here )
+    request['Ocp-Apim-Subscription-Key'] = @subscriptionKey
     request.body = "{}"
-
     response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
         http.request(request)
     end
     puts response.body
+    end
+
+    def getOperationStatus(operationId)
+        uri = URI('https://elevatorrecognition.cognitiveservices.azure.com/spid/v1.0/operations/' + @operationId)
+        uri.query = URI.encode_www_form({
+        })
+        request = Net::HTTP::Get.new(uri.request_uri)
+        request['Ocp-Apim-Subscription-Key'] = @subscriptionKey
+        request.body = "{}"
+        response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+            http.request(request)
+        end
+        puts response.body
     end
 
     def identification
@@ -65,6 +69,7 @@ end
     def upload
         # code to upload the audio file
     end 
+    
     def display
         # code to diplay the text
     end 
